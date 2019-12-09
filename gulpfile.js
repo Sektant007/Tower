bba8e35b7d27aaf8594e41cfba68551442a0b8c1
Sektant007 <dima.men@ro.ru>
2019-12-09 18:06:26 +0300

    feat: add gulp-sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,27 @@
- const gulp = require('gulp');
+ const {src, dest, watch} = require('gulp');
  const browserSync = require ('browser-sync').create();
-
- gulp.task('hello', function(done) {
-   console.log('Привет, мир!');
-   done();
-});
-
-
+ const sass = require('gulp-sass')
 
 // Static server
-gulp.task('browser-sync', function() {
+function ds() {
+  serveSass();
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-  gulp.watch("app/*.html").on('change', browserSync.reload);
-  
-});
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./*.js").on('change', browserSync.reload);
+
+};
+
+function serveSass() {
+  return src("./sass/*.sass")
+      .pipe(sass())
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
+
+exports.serve = ds;
+
